26f6d497755ee1633d7dc757419a50f6cf8f9a75
Reshmi Ramesh <reshmiramesh07@gmail.com>
2025-01-14 16:52:13 -0800

    added buttons for ui

--- script.js
+++ script.js
@@ -22,59 +22,54 @@ function getHumanChoice() {
   return hChoice;
 }
 
-function playGame() {
-  // score vars
-
-  let computerScore = 0;
-  let humanScore = 0;
-
-  function playRound(humanChoice, computerChoice) {
-    let verdict = humanChoice + " beats " + computerChoice;
-    let outcome = "lose";
-    if (humanChoice == "rock") {
-      if (computerChoice == "rock") {
-        outcome = "tie";
-      } else if (computerChoice == "paper") {
-        outcome = "lose";
-      } else if (computerChoice == "scissors") {
-        outcome = "win";
-      }
-    } else if (humanChoice == "paper") {
-      if (computerChoice == "rock") {
-        outcome = "win";
-      } else if (computerChoice == "paper") {
-        outcome = "tie";
-      } else if (computerChoice == "scissors") {
-        outcome = "lose";
-      }
-    } else if (humanChoice == "scissors") {
-      if (computerChoice == "rock") {
-        outcome = "lose";
-      } else if (computerChoice == "paper") {
-        outcome = "win";
-      } else if (computerChoice == "scissors") {
-        outcome = "tie";
-      }
+function playRound(humanChoice, computerChoice) {
+  let verdict = humanChoice + " beats " + computerChoice;
+  let outcome = "lose";
+  if (humanChoice == "rock") {
+    if (computerChoice == "rock") {
+      outcome = "tie";
+    } else if (computerChoice == "paper") {
+      outcome = "lose";
+    } else if (computerChoice == "scissors") {
+      outcome = "win";
     }
-
-    if (outcome == "lose") {
-      computerScore++;
-      verdict = computerChoice + " beats " + humanChoice + ".";
-      console.log("You " + outcome + "! " + verdict);
-    } else if (outcome == "tie") {
-      console.log("You tied!");
-    } else {
-      humanScore;
-      console.log("You " + outcome + "! " + verdict + ".");
+  } else if (humanChoice == "paper") {
+    if (computerChoice == "rock") {
+      outcome = "win";
+    } else if (computerChoice == "paper") {
+      outcome = "tie";
+    } else if (computerChoice == "scissors") {
+      outcome = "lose";
+    }
+  } else if (humanChoice == "scissors") {
+    if (computerChoice == "rock") {
+      outcome = "lose";
+    } else if (computerChoice == "paper") {
+      outcome = "win";
+    } else if (computerChoice == "scissors") {
+      outcome = "tie";
     }
   }
 
-  for (var i = 0; i < 5; i++) {
-    const humanSelection = getHumanChoice();
-    const computerSelection = getComputerChoice();
-    playRound(humanSelection, computerSelection);
-    console.log("C: " + computerScore + "; H: " + humanScore);
+  if (outcome == "lose") {
+    verdict = computerChoice + " beats " + humanChoice + ".";
+    console.log("You " + outcome + "! " + verdict);
+  } else if (outcome == "tie") {
+    console.log("You tied!");
+  } else {
+    console.log("You " + outcome + "! " + verdict + ".");
   }
 }
 
-playGame();
+// playGame();
+
+const btnRock = document.querySelector("#rock");
+const btnPaper = document.querySelector("#paper");
+const btnScissors = document.querySelector("#scissors");
+
+btnRock.addEventListener("click", playRound("rock", getComputerChoice()));
+btnPaper.addEventListener("click", playRound("paper", getComputerChoice()));
+btnScissors.addEventListener(
+  "click",
+  playRound("scissors", getComputerChoice())
+);
